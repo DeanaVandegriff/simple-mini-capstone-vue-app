1579a54e3c4ded92b6809edd049d64456e6c80fa
deana <dvandegriff1@gmail.com>
2022-04-13 14:28:42 -0500

    adds index and show actions

---
+++ src/views/ProductsIndex.vue
@@ -0,0 +1,29 @@
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      products: [],
+    };
+  },
+
+  created: function () {
+    axios.get("http://localhost:3000/products.json").then((response) => {
+      this.products = response.data;
+      console.log("all products", this.products);
+    });
+  },
+};
+</script>
+
+<template>
+  <h1>smcMart Products!</h1>
+  <div v-for="product in products" v-bind:key="product.id">
+    <h2>Name: {{ product.name }}</h2>
+    <img :src="product.image_url" v-bind:alt="product.title" />
+    <a v-bind:href="`/products/${product.id}`">More Info!</a>
+  </div>
+</template>
+
+<style></style>
---
+++ src/views/ProductsShow.vue
@@ -0,0 +1,25 @@
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      product: {},
+    };
+  },
+  created: function () {
+    axios.get("http://localhost:3000/product/" + this.$route.params.id + ".json").then((response) => {
+      console.log(response.data);
+      this.product = response.data;
+    });
+  },
+};
+</script>
+
+<template>
+  <h1>Product</h1>
+  <p>{{ product }}</p>
+  <a href="/products">Back to all Products</a>
+</template>
+
+<style></style>
